85e359be9d1b4215a0cb064b34dc4cb836e73bc4
Ilya Prymak <primak2506@gmail.com>
2024-08-01 15:19:09 +0300

    feat: footer and banner

--- src/components/common/Layout/index.tsx
+++ src/components/common/Layout/index.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import Banner from '@/components/containers/Banner';
+import Footer from '@/components/containers/Footer';
 import Header from '@/components/containers/Header';
 
 export default function Layout() {
   return (
     <>
       <Header />
+      <Banner />
       <Outlet />
+      <Footer />
     </>
   );
 }
