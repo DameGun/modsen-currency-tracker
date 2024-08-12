f63537e8d594a6d411275be0c8bff72d75dc82c1
Ilya Prymak <primak2506@gmail.com>
2024-08-12 23:43:26 +0300

    fix: theme switch test #3

--- cypress/e2e/ThemeSwitch.cy.ts
+++ cypress/e2e/ThemeSwitch.cy.ts
@@ -4,15 +4,15 @@ describe('ThemeSwitch spec', () => {
   });
 
   it('should have the correct initial theme', () => {
-    cy.get('html').should('have.attr', 'data-theme', 'dark');
+    cy.get('html').should('have.attr', 'data-theme', 'light');
   });
 
   it('should change theme when the button is clicked', () => {
-    cy.get('html').should('have.attr', 'data-theme', 'dark');
+    cy.get('html').should('have.attr', 'data-theme', 'light');
 
     cy.get('.theme-switch').click();
     cy.wait(2000);
 
-    cy.get('html').should('have.attr', 'data-theme', 'light');
+    cy.get('html').should('have.attr', 'data-theme', 'dark');
   });
 });
